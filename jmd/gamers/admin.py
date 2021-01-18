16980e1673a3818dd4654d3bf2538f815144e84a
RISHABH SIDANA <65805267+dinagon@users.noreply.github.com>
2021-01-18 12:55:49 +0530

    Add files via upload

---
+++ jmd/gamers/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from gamers.models import Contact,Game,Question
+
+# Register your models here.
+
+admin.site.register(Contact)
+admin.site.register(Game)
+admin.site.register(Question)
